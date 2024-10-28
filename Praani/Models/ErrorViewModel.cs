7fe6f50ef96638fe0a1deb4262739fdcfc0ecf2e
Karthi ck <karthi22308@gmail.com>
2024-10-28 19:56:39 +0530

    Add project files.

---
+++ Praani/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Praani.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
